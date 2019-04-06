d3fb664016e9eef590475f8af4eaa4d617533454
Bulat Khayrullin <habar.2704@mail.ru>
2019-04-06 11:29:02 +0300

    добавлены классы REST API

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,3 +5,6 @@ $route['default_controller'] = 'dashboard/index';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['api/authentication/login'] = 'api/authentication/login';
+$route['api/authentication/registration'] = 'api/authentication/registration';
+$route['api/authentication/user/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/authentication/user/id/$1/format/$3$4';
